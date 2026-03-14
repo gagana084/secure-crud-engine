35f5ef8c8792cc5ea29b310bd5784526472520fd
Gagana Randimal <randimalhennayaka@gmail.com>
2026-03-14 08:55:57 +0530

    Update Env.php

--- config/Env.php
+++ config/Env.php
@@ -1,1 +1,25 @@
 
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+class Env
+{
+    private static $loaded = false;
+
+    public static function load()
+    {
+        if (!self::$loaded) {
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+            $dotenv->load();
+            self::$loaded = true;
+        }
+    }
+
+    public static function get(string $key, $default = null)
+    {
+        self::load();
+        return $_ENV[$key] ?? $default;
+    }
+}
